b0aeaffba87eeed8f2cb6aac719a747debdf957a
Oyat0824 <langue0824@gmail.com>
2022-01-12 00:17:24 +0900

    Update 숫자야구.c

--- Simple Example/숫자야구.c
+++ Simple Example/숫자야구.c
@@ -3,58 +3,59 @@
 #include <time.h>
 
 int main(void) {
-	srand(time(NULL));	// 난수 시드 값 
-	
+	srand(time(NULL));
+
 	int input[3], comInput[3];
-	
+
 	int strike, ball, count = 0;
 	int i, j;
-	
+
 	// 컴퓨터 입력 
-	for(i = 0; i < 3; i++) {
+	for (i = 0; i < 3; i++) {
 		comInput[i] = rand() % 10;
-		
+
 		// 중복 값이면 값 재정의 
-		for(j = 0; j < i; j++) {
-			if(comInput[i] == comInput[j]) {
+		for (j = 0; j < i; j++) {
+			if (comInput[i] == comInput[j]) {
 				i--;
 				break;
 			}
 		}
 	}
-	
-	while(1) {
+
+	while (1) {
 		strike = 0;
 		ball = 0;
-			
+
 		// 유저 입력 
 		printf("[0 ~ 9] 숫자를 3개 입력해주세요.\n");
 		printf(">> ");
 		scanf("%d %d %d", &input[0], &input[1], &input[2]);
-		
-		if(input[0] <= 0 || input[0] >= 10 || input[1] <= 0 || input[1] >= 10 || input[2] <= 0 || input[2] >= 10) {
+
+		if (input[0] < 0 || input[0] >= 10 || input[1] < 0 || input[1] >= 10 || input[2] < 0 || input[2] >= 10) {
 			printf("\n▶  숫자를 다시 입력해주세요.\n\n");
 			continue;
 		}
-		
+
 		// 검수 
-		for(i = 0; i < 3; i++) {
-			if(input[i] == comInput[i]) {
+		for (i = 0; i < 3; i++) {
+			if (input[i] == comInput[i]) {
 				strike++;
-			} else {	
-				for(j = 0; j < 3; j++) {
-					if(input[i] == comInput[j]) {
+			}
+			else {
+				for (j = 0; j < 3; j++) {
+					if (input[i] == comInput[j]) {
 						ball++;
 					}
 				}
 			}
 		}
-		
+
 		count++;
-		
+
 		printf("\n▶  Strike : %d , Ball : %d\n\n", strike, ball);
-		
-		if(strike == 3) {
+
+		if (strike == 3) {
 			printf(">> %d번만에 클리어하셨습니다!\n", count);
 			break;
 		}
